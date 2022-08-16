aa4ce716b6f855db39fdb2e1db56b28da0258c74
rodrigofercav <rodrigofercav@gmail.com>
2022-08-15 22:58:51 -0300

    initial commit.

---
+++ src/main/java/dio/java/desafio/springboot/pilates/model/Instrutor.java
@@ -0,0 +1,11 @@
+package dio.java.desafio.springboot.pilates.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Instrutor {
+    
+    private Long id;
+    private String nome;
+
+}
---
+++ src/test/java/dio/java/desafio/springboot/pilates/PilatesApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.java.desafio.springboot.pilates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
